35d64a4e097fea2045d4882e0484898eb04c5f95
Taki Academy <takiacademy@alis-Mac-mini.local>
2023-05-25 16:49:35 +0100

    fix / pdf display ( still no css for now  )

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import PdfApp from './components/pdfApp';
 import { pdfjs } from 'react-pdf';
-
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.js',
   import.meta.url,
