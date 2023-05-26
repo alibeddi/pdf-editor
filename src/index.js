ae4f6004624f83ff6f60aa02085b482c40b1ddcb
Taki Academy <takiacademy@alis-Mac-mini.local>
2023-05-26 14:55:28 +0100

    remove changes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.js',
   import.meta.url,
 ).toString();
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
